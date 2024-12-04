a2f2de227460b3f70666fe68473fc9669ee497a9
Raulro03 <raulromeraortiz@gmail.com>
2024-12-04 16:06:48 +0100

    Paso 5 mejorar test con una descripcion

--- database/migrations/2024_12_04_144421_create_courses_table.php
+++ database/migrations/2024_12_04_144421_create_courses_table.php
@@ -11,6 +11,7 @@ return new class extends Migration {
             $table->id();
             $table->string('title');
             $table->timestamps();
+            $table->text('description');
         });
     }
 
--- tests/Feature/PagesHomeTest.php
+++ tests/Feature/PagesHomeTest.php
@@ -8,15 +8,19 @@ uses(RefreshDatabase::class);
 
 it('Show courses overview', function () {
     // Arrange (Lo que necesito para hacer el test)
-    Course::factory()->create(['title' => 'Course A']);
-    Course::factory()->create(['title' => 'Course B']);
-    Course::factory()->create(['title' => 'Course C']);
+    Course::factory()->create(['title' => 'Course A', 'description' => 'Description Course A']);
+    Course::factory()->create(['title' => 'Course B', 'description' => 'Description Course B']);
+    Course::factory()->create(['title' => 'Course C', 'description' => 'Description Course C']);
 
     // Act (Accion que hara, lo que hago en el test)
     get(route('home'))
         ->assertSeeText([
             'Course A',
+            'Description Course A',
             'Course B',
+            'Description Course B',
+            'Course C',
+            'Description Course C',
         ]);
 
     // Assert (Afirmar, lo que me permite verificar, comprobaciones)
