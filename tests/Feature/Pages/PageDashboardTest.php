f99ebd328503dcc2389c51213e8a8fdd9098adb5
Raulro03 <raulromeraortiz@gmail.com>
2024-12-11 16:28:13 +0100

    Proyecto  Error version de laravel mix o vite o lo arreglo o me clono el del profesor

---
+++ tests/Feature/Pages/PageDashboardTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+it('cannot be accessed by guest', function () {
+    //Arrange
+
+
+    //Act
+
+
+    //Assert
+
+
+});
+it('lists purchased courses', function () {
+    //Arrange
+
+
+    //Act
+
+
+    //Assert
+
+
+});
+it('does not list other courses', function () {
+    //Arrange
+
+
+    //Act
+
+
+    //Assert
+
+
+});
+it('shows latest purchased course first', function () {
+    //Arrange
+
+
+    //Act
+
+
+    //Assert
+
+
+});
+it('includes link to product videos', function () {
+    //Arrange
+
+
+    //Act
+
+
+    //Assert
+
+
+});
+
